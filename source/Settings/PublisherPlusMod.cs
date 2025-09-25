65d6a696b4f735979c66919e9fb8a72650d9f5cb
Nightmare Corporation <12363909-nightmarecorporation@users.noreply.gitlab.com>
2025-09-25 14:20:10 +0200

    feat: setting for temp folder path

--- source/Settings/PublisherPlusMod.cs
+++ source/Settings/PublisherPlusMod.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using RimWorld;
+using System.IO;
+using UnityEngine;
 using Verse;
 
 namespace PublisherPlus.Settings
@@ -24,7 +26,57 @@ namespace PublisherPlus.Settings
 			list.CheckboxLabeled(Language.Get("Settings.ShowFileSize"), ref PublisherPlusSettings.ShowFileSizeInFileTree);
 			list.CheckboxLabeled(Language.Get("Settings.IndentButtonsWithTree"), ref PublisherPlusSettings.IndentButtonsWithTree, Language.Get("Settings.IndentButtonsWithTree.Tip"));
 
+			DoTempFolder(list);
+
 			list.End();
+		}
+
+		static string tempFolderText = PublisherPlusSettings.TempFolderPath;
+		private void DoTempFolder(Listing_Standard list)
+		{
+			list.Label(Language.Get("Settings.CurrentTempPath", tempFolderText.Italic()));
+			RectDivider divider = new RectDivider(list.GetRect(Text.LineHeight), this.GetType().GetHashCode());
+			DoSave();
+			DoReset();
+            tempFolderText = Widgets.TextArea(divider.Rect, tempFolderText);
+
+			void DoSave()
+			{
+				string label = Language.Get("Settings.Save");
+				Rect labelRect = divider.NewCol(Text.CalcSize(label + "    ").x, HorizontalJustification.Right, 2).Rect;
+				if(Widgets.ButtonText(labelRect, label))
+				{
+					TrySetTempFolder();
+				}
+			}
+
+			void DoReset()
+            {
+                string label = Language.Get("Settings.Reset");
+                Rect labelRect = divider.NewCol(Text.CalcSize(label + "    ").x, HorizontalJustification.Right, 2).Rect;
+                if(Widgets.ButtonText(labelRect, label))
+                {
+					tempFolderText = GenFilePaths.TempFolderPath;
+					TrySetTempFolder();
+                }
+            }
+		}
+
+		private void TrySetTempFolder()
+		{
+			if(!Directory.Exists(tempFolderText))
+			{
+                Dialog_MessageBox messageBox = new Dialog_MessageBox(Language.Get("Settings.FolderDoesNotExist"),
+					Language.Get("Settings.FolderDoesNotExist.CreateFolder"), () =>
+					{
+						Directory.CreateDirectory(tempFolderText);
+						TrySetTempFolder();
+					},
+					Language.Get("Settings.Cancel"), () => { });
+				Find.WindowStack.Add(messageBox);
+				return;
+			}
+			PublisherPlusSettings.TempFolderPath = tempFolderText;
 		}
 	}
 
@@ -33,6 +85,7 @@ namespace PublisherPlus.Settings
 		public static bool UseRelativePathToParentForFileTree = true;
 		public static bool ShowFileSizeInFileTree = true;
 		public static bool IndentButtonsWithTree = true;
+		public static string TempFolderPath = GenFilePaths.TempFolderPath;
 
 		public override void ExposeData()
 		{
@@ -40,6 +93,7 @@ namespace PublisherPlus.Settings
 			Scribe_Values.Look(ref UseRelativePathToParentForFileTree, nameof(UseRelativePathToParentForFileTree));
 			Scribe_Values.Look(ref ShowFileSizeInFileTree, nameof(ShowFileSizeInFileTree));
 			Scribe_Values.Look(ref IndentButtonsWithTree, nameof(IndentButtonsWithTree));
+			Scribe_Values.Look(ref TempFolderPath, nameof(TempFolderPath));
 		}
 	}
 }
